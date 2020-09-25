915988f3bb30269265551ee69ed88ffe83c9832b
EmilyStieben <emily.stieben@gmail.com>
2020-09-24 21:36:37 -0400

    added new classes

--- board.py
+++ board.py
@@ -1,11 +1,48 @@
 class Board:
+#adds board
+   def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.data = [] 
+    
+        for row in range( self.height ):
+            boardRow = []
+            for col in range( self.width ):
+                boardRow += [' ']
+            self.data += [boardRow]
+        
+        
+def __repr__(self):
+        s = ''
+        for row in range( self.height ):
+            s += '|'
+            for col in range( self.width ):
+                s += self.data[row][col] + '|'
+            s += '\n'
+            
+        s += '--'*self.width + '-\n'
+        
+        for col in range( self.width ):
+            s += ' ' + str(col % 10)
+        s += '\n'
+        
+        return s
+                                    
+def addMove( self,col,ox ):
+        if self.allowsMove(col):
+            for row in range( self.height ):
+                if self.data[row][col] != ' ':
+                    self.data[row-1][col] = ox
+                    return
+            self.data[self.height-1][col] = ox #if empty column, place piece at the bottom
+#check if move is allowed,
+def allowsMove( self,col ):
+        if 0 <= col < self.width:
+            return self.data[0][col] == ' '
+        else:
+            print "try a valid number"
 
-	def __init__(self):
-
-		self.board = # to do initialize board
-
-
-	def getWinner(self):	
+def getWinner(self):	
 
 		# checks for row of 4
 		for y in range(0, 6):
@@ -52,4 +89,9 @@ class Board:
 					continue
 
 				found = True
+				
 		
+#move
+
+#check for valid move
+#print board
