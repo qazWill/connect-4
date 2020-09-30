1613acb5013f9758d0a39697adb5ba535d7a14c6
Ashwin Kumar <ashwinkumar3399@gmail.com>
2020-10-01 01:34:08 +0530

    updated board, added helper

--- helpers.py
+++ helpers.py
@@ -4,6 +4,29 @@ for use in the connect 4 game
 
 '''
 
+# Returns whether there are n consecutive non-zero tokens
+# Returns token id (int) if found, else returns 0
+def checkConsecutive(aList, n):
+    if len(aList) < n:
+        return 0
+    # Set 1st element as prev
+    count = 1
+    prev = aList[0]  
+    for i in range(1, len(aList)):
+        curr = aList[i]
+        if curr == 0:
+            count = 0
+        elif prev != curr:
+            count = 1
+        else:
+            count += 1
+        # Check Count
+        if count >= n:
+            return prev
+        # Set new prev
+        prev = curr
+    return 0
+
 
 # returns winner if found
 # returns 0 if no winner is found
--- player.py
+++ player.py
@@ -1,3 +1,14 @@
-class Player:
+'''
+Base CLass for Player. It will split into human and AI implementations
+'''
+class BasePlayer:
+	def __init__(self, name, color):
+		self.name = name
+		self.color = color
 
-	def __init__(self):
+	def __repr__(self):
+		return str(self.name)
+
+	def play(self, board):
+		return True
+
