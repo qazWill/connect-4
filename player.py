ac9754f36a65f95d77c1189bc1fbfe7dd6e4e9f6
Ashwin Kumar <ashwinkumar3399@gmail.com>
2020-10-01 18:54:41 +0530

    updated player class

--- player.py
+++ player.py
@@ -1,14 +1,20 @@
+from board import Board
+
 '''
 Base CLass for Player. It will split into human and AI implementations
+
+token_id (int) is the token used to represent this player's token on the board
 '''
 class BasePlayer:
-	def __init__(self, name, color):
+	def __init__(self, name, color, token_id):
 		self.name = name
 		self.color = color
+		self.token_id = token_id
 
 	def __repr__(self):
 		return str(self.name)
 
-	def play(self, board):
-		return True
-
+	def playMove(self, board, col):
+		if board.dropToken(col, self.token_id):
+			return True
+		return False
